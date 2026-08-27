e22d72638c8b43bbecb5c6f8c2e8f1cd71bda7bf
rahuljap <rahuljap86@gmail.com>
2026-08-26 22:10:33 -0500

    tab keybinding changes

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -18,6 +18,15 @@ vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window'
 -- gt = Go Tab (Next)
 vim.keymap.set("n", "<A-j>", "gT", { desc = "Prev tab" })
 vim.keymap.set("n", "<A-k>", "gt", { desc = "Next tab" })
+-- Close the current tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<cr>", { desc = "Close current tab" })
+-- Open a new tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "Open new tab" })
+-- Move to next tab
+vim.keymap.set("n", "<leader>tl", "<cmd>tabnext<cr>", { desc = "Go to next tab" })
+-- Move to previous tab
+vim.keymap.set("n", "<leader>th", "<cmd>tabprevious<cr>", { desc = "Go to previous tab" })
+
 
 -- Map Alt + 1-9 to go to tab 1-9
 for i = 1, 9 do
